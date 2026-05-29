39bb34d4ec1b0d3f0f58a74d3279522172566fa3
chitose <103087652@qq.com>
2026-05-29 13:57:46 +0800

    feat: enhance SoloRaid recognition with red dot detection and update recognition logic

--- agent/go-service/custom-recognizer/soloraid/action.go
+++ agent/go-service/custom-recognizer/soloraid/action.go
@@ -21,16 +21,30 @@ func (r *SoloRaidRecognitionRunner) Run(ctx *maa.Context, arg *maa.CustomRecogni
 		return nil, false
 	}
 	log.Debug().Str("custom param", arg.CustomRecognitionParam).Interface("roi", arg.Roi).Msg("Running SoloRaidRecognition")
-	result, err := ctx.RunRecognitionDirect(maa.RecognitionTypeOCR, maa.OCRParam{
+	textResult, err := ctx.RunRecognitionDirect(maa.RecognitionTypeOCR, maa.OCRParam{
 		ROI:      maa.NewTargetRect(arg.Roi),
 		Expected: []string{param.Expected},
 	}, arg.Img)
-	if err != nil {
+	if err != nil || textResult.Results.Best == nil {
 		return nil, false
 	}
-	log.Debug().Interface("ocr result box", result.Box).Msg("OCR recognition completed in SoloRaidRecognition")
+	log.Debug().Interface("ocr result box", textResult.Box).Interface("ocr best result", textResult.Results.Best).Msg("OCR recognition completed in SoloRaidRecognition")
+	textBox := textResult.Box
+	redDotRoi := maa.NewTargetRect(maa.Rect{textBox.X(), textBox.Y() - 60, textBox.Width() + 20, textBox.Height() + 60})
+	log.Debug().Interface("red dot roi", redDotRoi).Msg("Checking for red dot in SoloRaidRecognition")
+	redDotResult, err := ctx.RunRecognitionDirect(maa.RecognitionTypeTemplateMatch, maa.TemplateMatchParam{
+		ROI: redDotRoi,
+		Template: []string{
+			"Common/RedDot.png",
+		},
+	}, arg.Img)
+	if err != nil || redDotResult.Results.Best == nil {
+		return nil, false
+	}
+	log.Debug().Interface("red dot recognition box", redDotResult.Box).Interface("red dot recognition best result", redDotResult.Results.Best).Msg("Red dot recognition completed in SoloRaidRecognition")
+	clickBox := maa.Rect{redDotResult.Box.X() - 18, redDotResult.Box.Y() + 16, 8, 8}
 	return &maa.CustomRecognitionResult{
-		Box:    result.Box,
-		Detail: result.Name,
+		Box:    clickBox,
+		Detail: "SoloRaid recognized, click the red dot",
 	}, true
 }
